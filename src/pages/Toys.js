404b072f64f75d0ee4411f1fb879abb2ac6036a0
Destiny Okonkwo <dezok@unc.edu>
2023-02-18 16:36:44 -0500

    Copied over Toy Catolog

--- src/pages/Toys.js
+++ src/pages/Toys.js
@@ -2,6 +2,45 @@ import React from 'react'
 
 export default function Toys() {
   return (
-    <div>Toys</div>
+    <div>
+      <div>
+        <img src = {require('../images/Home/Late Night with Toys (9.9.22)/Copy of R1-07720-020A.JPG')}/>
+      </div>
+      <h1>Toys</h1>
+      <h3>Adapted Toys</h3>
+      <div>Click on the photos to view the toy guides</div>
+      <div id = 'catalog-pictures'>
+        <a href = 'https://docs.google.com/presentation/d/1wbJqiEVo8fUr-7MK_vaexr9-cqTBe6HjSzHXuhyEOuY/edit#slide=id.p'>
+          <img src = {require('../images/Toy Catolog/Copy of trex.jpg')} alt = 'Modified T-rex Toy'/>
+        </a>
+        <a href = 'https://docs.google.com/presentation/d/1sG6zYR71rNoACMY5j51roubwaqilKjNm_EgJfxFn7VU/edit#slide=id.p'>
+          <img src = {require('../images/Toy Catolog/Copy of airplane.jpg')} alt = 'Modified Plane Toy'/>
+        </a>
+        <a href = 'https://docs.google.com/presentation/d/12rRDEw87h6ICf5L7guNQBBB5o5kUtJ3QybOLMZ_QNRg/edit?usp=sharing'>
+          <img src = {require('../images/Toy Catolog/Copy of garbage.jpg')} alt = 'Modified Truck Toy'/>
+        </a>
+          <img src = {require('../images/Toy Catolog/Copy of bus.jpg')} alt = 'Modified School Bus Toy'/> 
+          <img src = {require('../images/Toy Catolog/Copy of snake.jpg')} alt = 'Modified Snake Toy'/> 
+        <a href = 'https://docs.google.com/presentation/d/1wNAGJYabdsUwAWgdzBGDGSQVSf8A0drX-7Vv6Ua_UU4/edit#slide=id.ge901fea6e3_0_80'>
+          <img src = {require('../images/Toy Catolog/Copy of Edited_Dd.jpg')} alt = 'Modified Automobile Toy'/> 
+        </a>
+        <a href = 'https://docs.google.com/presentation/d/1rYG7PFngmJgI7uBpoO3ydhBF3RiEJJCjJ8ZIl-tqLXY/edit#slide=id.p'>
+          <img src = {require('../images/Toy Catolog/Copy of Edited_Ft.jpg')} alt = 'Modified Fire Truck Toy'/>
+        </a>
+        <a href = 'https://docs.google.com/presentation/d/1wNAGJYabdsUwAWgdzBGDGSQVSf8A0drX-7Vv6Ua_UU4/edit#slide=id.p'>
+          <img src = {require('../images/Toy Catolog/Copy of Edited_Tractor.jpg')} alt = 'Modified Tractor Toy'/> 
+        </a>
+          <img src = {require('../images/Toy Catolog/Copy of Edited_Lizard.jpg')} alt = 'Modified Lizard Toy'/> 
+        <a href = 'https://docs.google.com/document/d/1rYR7cj2QgniXExzF7qxCrk9XsvUmf7qV7L0kWtusydA/edit'>
+          <img src = {require('../images/Toy Catolog/Copy of Edited_Penguin.jpg')} alt = 'Modified Penguin Slide Toy'/> 
+        </a>
+          <img src = {require('../images/Toy Catolog/Copy of Edited_Alien.jpg')} alt = 'Modified Alien Toy'/> 
+          <img src = {require('../images/Toy Catolog/Copy of Edited_Dog.jpg')} alt = 'Modified Dog Toy'/> 
+          <img src = {require('../images/Toy Catolog/Copy of Edited_Pixie.jpg')} alt = 'Modified Pixie Toy'/>
+      </div>
+      <div>
+        <img src = {require('../images/logo.png')} alt = 'CATCH Logo'/>
+      </div>
+    </div>
   )
 }
