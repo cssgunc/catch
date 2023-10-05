227db96c7e22903978381774bb229d44db2ff44b
Elaine Dong <elaine13@email.unc.edu>
2023-10-05 17:16:20 -0400

    make spacing responsive

--- src/pages/About.js
+++ src/pages/About.js
@@ -56,99 +56,104 @@ export default function About(){
         imagePath={bannerImage}
         title='About'
       />
-      <div className="intro">
-        <h2>Carolina Adapts Toys for Children</h2>
-        <a id="about-video">
-          <iframe title="Intro Video" class = "video" width = "750" height = "450" sandbox="allow-scripts allow-popups allow-forms allow-same-origin allow-popups-to-escape-sandbox allow-downloads allow-modals" frameborder="0" aria-label="YouTube Video, Making toys more accessible for kids" src="https://www.youtube.com/embed/LDH36tCupQQ" allowfullscreen=""></iframe>
-        </a>
-        <div id = "text">
-          <p font= "font-family: Arial"><em>
-            "Founded in November 2018, CATCH works to provide adapted toys to children with special needs who are unable to play with most commercially manufactured toys. Utilizing engineering, creativity, and innovation, we give back to the community, while also raising awareness about underrepresented communities and their needs."
-          </em></p>
+      <div className="about-content">
+        <div className="intro">
+          <h2>Carolina Adapts Toys for Children</h2>
+          <a id="about-video" href="#">
+            <div className="video-container">
+              <iframe title="Intro Video" class = "video" width = "750" height = "450" sandbox="allow-scripts allow-popups allow-forms allow-same-origin allow-popups-to-escape-sandbox allow-downloads allow-modals" frameborder="0" aria-label="YouTube Video, Making toys more accessible for kids" src="https://www.youtube.com/embed/LDH36tCupQQ" allowfullscreen=""></iframe>
+            </div>
+          </a>
+          <div id = "text">
+            <p font= "font-family: Arial"><em>
+              "Founded in November 2018, CATCH works to provide adapted toys to children with special needs who are unable to play with most commercially manufactured toys. Utilizing engineering, creativity, and innovation, we give back to the community, while also raising awareness about underrepresented communities and their needs."
+            </em></p>
+          </div>
+        </div>
+        
+        <h2>FAQ</h2>
+        <FAQ>
+          <FAQ.QAItem>
+            <FAQ.Question answerId="q1">
+              {(isOpen, onToggle) => {
+                return (
+                  <><hr></hr>
+                    <span>How do I join CATCH?</span>
+                    <hr></hr>
+                  </>
+                );
+              }}
+            </FAQ.Question>
+            <FAQ.Answer  id="q1">Anyone (in any major) can join CATCH at any point in the year. Just come to one of our meetings to start adapting! (To become an active member, there are certain requirements like GBM attendance.)</FAQ.Answer>
+          </FAQ.QAItem>
+          <FAQ.QAItem>
+            <FAQ.Question answerId="q2">
+              {(isOpen, onToggle) => {
+                return (
+                  <>
+                    {isOpen ? "" : ""}
+                    <span>Do I need any experience to join CATCH?</span>
+                    <hr></hr>
+                  </>
+                );
+              }}
+            </FAQ.Question>
+            <FAQ.Answer id="q2">Nope, we welcome all experience levels. We also provide soldering training to beginners at every meeting.</FAQ.Answer>
+          </FAQ.QAItem>
+          <FAQ.QAItem>
+            <FAQ.Question answerId="q3">
+              {(isOpen, onToggle) => {
+                return (
+                  <>
+                    {isOpen ? "" : ""}
+                    <span>When are the meetings?</span>
+                    <hr></hr>
+                  </>
+                );
+              }}
+            </FAQ.Question>
+            <FAQ.Answer id="q3"> We meet every Tuesday from 6:30 PM to 8:00 PM.</FAQ.Answer>
+          </FAQ.QAItem>
+          
+          <FAQ.QAItem>
+            <FAQ.Question answerId="q3">
+              {(isOpen, onToggle) => {
+                return (
+                  <>
+                    {isOpen ? "" : ""}
+                    <span>Where are the meetings?</span>
+                    <hr></hr>
+                  </>
+                );
+              }}
+            </FAQ.Question>
+            <FAQ.Answer id="q3"> We meet in Phillips Hall 19B (the basement of Phillips).</FAQ.Answer>
+          </FAQ.QAItem>
+
+          <FAQ.QAItem>
+            <FAQ.Question answerId="q3">
+              {(isOpen, onToggle) => {
+                return (
+                  <>
+                    {isOpen ? "" : ""}
+                    <span>How can I donate?</span>
+                    <hr></hr>
+                  </>
+                );
+              }}
+            </FAQ.Question>
+            <FAQ.Answer id="q3"> Thanks for considering donating to us! All funds go to toy purchases which we will adapt for free and donate to a series of our community partners. Check out our <a href="https://www.gofundme.com/f/carolina-adapts-toys-for-children?utm_campaign=p_cf+share-flow-1&utm_medium=copy_link&utm_source=customer">GoFundMe</a> </FAQ.Answer>
+          </FAQ.QAItem>
+        </FAQ>
+
+        <div className="execs">
+          <h2 id = 'title'> Our Executive Board </h2>
+          <div id = 'table'>
+            <ExecGrid id="exec-team"/>
+          </div>
         </div>
       </div>
       
-      <h2>FAQ</h2>
-      <FAQ>
-        <FAQ.QAItem>
-          <FAQ.Question answerId="q1">
-            {(isOpen, onToggle) => {
-              return (
-                <><hr></hr>
-                  <span>How do I join CATCH?</span>
-                  <hr></hr>
-                </>
-              );
-            }}
-          </FAQ.Question>
-          <FAQ.Answer  id="q1">Anyone (in any major) can join CATCH at any point in the year. Just come to one of our meetings to start adapting! (To become an active member, there are certain requirements like GBM attendance.)</FAQ.Answer>
-        </FAQ.QAItem>
-        <FAQ.QAItem>
-          <FAQ.Question answerId="q2">
-            {(isOpen, onToggle) => {
-              return (
-                <>
-                  {isOpen ? "" : ""}
-                  <span>Do I need any experience to join CATCH?</span>
-                  <hr></hr>
-                </>
-              );
-            }}
-          </FAQ.Question>
-          <FAQ.Answer id="q2">Nope, we welcome all experience levels. We also provide soldering training to beginners at every meeting.</FAQ.Answer>
-        </FAQ.QAItem>
-        <FAQ.QAItem>
-          <FAQ.Question answerId="q3">
-            {(isOpen, onToggle) => {
-              return (
-                <>
-                  {isOpen ? "" : ""}
-                  <span>When are the meetings?</span>
-                  <hr></hr>
-                </>
-              );
-            }}
-          </FAQ.Question>
-          <FAQ.Answer id="q3"> We meet every Tuesday from 6:30 PM to 8:00 PM.</FAQ.Answer>
-        </FAQ.QAItem>
-        
-        <FAQ.QAItem>
-          <FAQ.Question answerId="q3">
-            {(isOpen, onToggle) => {
-              return (
-                <>
-                  {isOpen ? "" : ""}
-                  <span>Where are the meetings?</span>
-                  <hr></hr>
-                </>
-              );
-            }}
-          </FAQ.Question>
-          <FAQ.Answer id="q3"> We meet in Phillips Hall 19B (the basement of Phillips).</FAQ.Answer>
-        </FAQ.QAItem>
-
-        <FAQ.QAItem>
-          <FAQ.Question answerId="q3">
-            {(isOpen, onToggle) => {
-              return (
-                <>
-                  {isOpen ? "" : ""}
-                  <span>How can I donate?</span>
-                  <hr></hr>
-                </>
-              );
-            }}
-          </FAQ.Question>
-          <FAQ.Answer id="q3"> Thanks for considering donating to us! All funds go to toy purchases which we will adapt for free and donate to a series of our community partners. Check out our <a href="https://www.gofundme.com/f/carolina-adapts-toys-for-children?utm_campaign=p_cf+share-flow-1&utm_medium=copy_link&utm_source=customer">GoFundMe</a> </FAQ.Answer>
-        </FAQ.QAItem>
-      </FAQ>
-
-      <div className="execs">
-        <h2 id = 'title'> Our Executive Board </h2>
-        <div id = 'table'>
-          <ExecGrid id="exec-team"/>
-        </div>
-      </div>
     </div>
   );
 }
