37de8db72b702efaf71107e84f90d0a5693de696
Tanvi Pulipaka <107874654+tanvip9@users.noreply.github.com>
2023-10-20 14:51:37 -0400

    Update toys (#43)
    * adjust text
    
    * make introduction section responsive
    
    * redid page
    
    * make spacing responsive
    
    * Fixed Exec Pics Mobility
    
    * Fixed Exec Pics Mobility
    
    * Fixed Code Formatting
    
    * Finished Phase 2
    
    * Fixed small bug that caused "Add to Cart" to stop displaying when toy was clicked
    
    ---------
    
    Co-authored-by: Elaine Dong <elaine13@email.unc.edu>
    Co-authored-by: Nirmal Rajendran <nirmal@ad.unc.edu>
    Co-authored-by: Nirmal <107888118+nirmalunc@users.noreply.github.com>

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -9,7 +9,7 @@ import Toys from '../../pages/Toys';
 import Donations from '../../pages/Donations';
 import MediaCoverage from '../../pages/MediaCoverage';
 import ShoppingCart from './ShoppingCart';
-import { toyInfo } from '../toyInfo';
+import { recentToys } from '../toyInfo';
 
 import { db } from '../../firebase-config'; // Fixing the import path
 import { addDoc, collection, getDoc, doc, updateDoc, serverTimestamp } from '@firebase/firestore'; // importing Firestore functions
@@ -21,7 +21,7 @@ function CartItem(props) {
   const [quantity, setQuantity] = useState(props.toy.quantity)
   const [disable, setDisable] = useState(quantity === 1)
   const toyName = props.toy.name;
-  const toy = toyInfo.find(item => item.name === toyName)
+  const toy = recentToys.find(item => item.name === toyName)
 
   const addOne = () => {
     let tempOrder = [...props.order];
