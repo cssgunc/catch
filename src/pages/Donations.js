cb336e59b8cb8acc3b0c5e0769a88cb97aaed7f5
jasonyi-unc <jasonyi@unc.edu>
2023-10-22 22:33:12 -0400

    Added slideshow on Donations page

--- src/pages/Donations.js
+++ src/pages/Donations.js
@@ -7,6 +7,8 @@ import org3Image from '../images/Donations/donation3.jpg';
 import org4Image from '../images/Donations/donation4.jpg';
 import org5Image from '../images/Donations/donation5.jpg';
 import org6Image from '../images/Donations/donation6.jpg';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
 
 import "./Donations.css";
 
@@ -14,18 +16,9 @@ import "./Donations.css";
 
 function DonationBox(props) {
   return (
-    <div class="sitesection">
-      {props.side ? <img
-        src={props.imagePath}
-        class="pictures"
-        alt={"Donation to " + props.organization}
-        style={{
-          maxWidth: "40%",
-          maxHeight: "auto",
-        }}
-      /> : <></>}
+    <div className="sitesection">
       <div
-        class="text-box"
+        className="text-box"
         style={{
           paddingLeft: "3%",
         }}
@@ -33,10 +26,10 @@ function DonationBox(props) {
         <h3>
           <b>{props.organization}</b>
         </h3>
-        { props.description !== '' ?
+        {props.description !== '' &&
           <div class="description">
             {props.description}
-          </div> : <></>
+          </div>
         }
         <div>
           <strong>Total Toys Donated: </strong>{props.total}
@@ -45,42 +38,42 @@ function DonationBox(props) {
           <strong>Total Donation: </strong>{props.donations}
         </div>
       </div>
-      {!props.side ? <img
+      <img
         src={props.imagePath}
-        class="pictures"
+        className="pictures"
         alt={"Donation to " + props.organization}
         style={{
           maxWidth: "40%",
           maxHeight: "auto",
         }}
-      /> : <></>}
+      />
     </div>
   );
 }
 
-function DonationDisplay (props) {
+function DonationDisplay() {
   const donationsInfo = [
-    {side: true, imagePath: org1Image, organization: 'Carolina Institute for Developmental Disabilities', total: 20, donations: 2, description: "The Carolina Institute for Developmental Disabilities is a comprehensive program for services, research, and training relevant to individuals with developmental disabilities and their families. The Carolina Institute provides a continuum of clinical services from complex, interdisciplinary evaluations on-site to more limited and selected clinical services and training in all 100 counties in North Carolina. The Institute brings together state-of-the-art research and clinical practice to ensure the best possible care for citizens of North Carolina."},
-    {side: false, imagePath: org2Image, organization: 'UNC Center for Rehabilitative Care', total: 14, donations: 2, description: 'The mission of the UNC Inpatient Rehabilitation Center is to improve, restore and maintain functional abilities and maximize quality of life in patients with disabilities; educate health care professionals in rehabilitation care and services; and advance rehabilitation research. Rehabilitative care provides persons served with the skills and support necessary to function in an environment with as much independence and choice and as little supervision and restriction as possible. The totality of this care spans the rehabilitation continuum to optimize the functionality and quality of life and prevent and or treat conditions of physically disabled persons.'},
-    {side: true, imagePath: org3Image, organization: "Levine Children's Hospital", total: 16, donations: 2, description: ''},
-    {side: false, imagePath: org4Image, organization: 'Novant Health', total: 10, donations: 1, description: ''},
-    {side: true, imagePath: org5Image, organization: 'Barton Pond Elementary School', total: 10, donations: 1, description: ''},
-    {side: false, imagePath: org6Image, organization: 'Aversboro Elementary School', total: 10, donations: 1, description: ''}
+    { imagePath: org1Image, organization: 'Carolina Institute for Developmental Disabilities', total: 20, donations: 2, description: "The Carolina Institute for Developmental Disabilities is a comprehensive program for services, research, and training relevant to individuals with developmental disabilities and their families. The Carolina Institute provides a continuum of clinical services from complex, interdisciplinary evaluations on-site to more limited and selected clinical services and training in all 100 counties in North Carolina. The Institute brings together state-of-the-art research and clinical practice to ensure the best possible care for citizens of North Carolina." },
+    { imagePath: org2Image, organization: 'UNC Center for Rehabilitative Care', total: 14, donations: 2, description: 'The mission of the UNC Inpatient Rehabilitation Center is to improve, restore and maintain functional abilities and maximize quality of life in patients with disabilities; educate health care professionals in rehabilitation care and services; and advance rehabilitation research. Rehabilitative care provides persons served with the skills and support necessary to function in an environment with as much independence and choice and as little supervision and restriction as possible. The totality of this care spans the rehabilitation continuum to optimize the functionality and quality of life and prevent and or treat conditions of physically disabled persons.' },
+    { imagePath: org3Image, organization: "Levine Children's Hospital", total: 16, donations: 2, description: '' },
+    { imagePath: org4Image, organization: 'Novant Health', total: 10, donations: 1, description: '' },
+    { imagePath: org5Image, organization: 'Barton Pond Elementary School', total: 10, donations: 1, description: '' },
+    { imagePath: org6Image, organization: 'Aversboro Elementary School', total: 10, donations: 1, description: '' }
   ]
 
   return (
-    <>
+    <Slide>
       {donationsInfo.map((donation) => (
-          <DonationBox
-            imagePath={donation.imagePath}
-            organization={donation.organization}
-            total={donation.total}
-            donations={donation.donations}
-            description={donation.description}
-            side={donation.side}
-          />
+        <DonationBox
+          imagePath={donation.imagePath}
+          organization={donation.organization}
+          total={donation.total}
+          donations={donation.donations}
+          description={donation.description}
+          side={donation.side}
+        />
       ))}
-    </>
+    </Slide>
   );
 }
 
@@ -89,7 +82,7 @@ export default function Donations() {
   const formUrl1 =
     "https://docs.google.com/forms/d/e/1FAIpQLSfOhVwyU37XieVYEo73C-MyJ1XbY_Hfcy-VB3D31d7F2Tf0Qg/viewform";
   const formUrl2 =
-    "https://docs.google.com/forms/d/e/1FAIpQLSfACZzhpliXiEolrF0IDf89XFW_RHx7DaSZkDeDLLF618HE1A/viewform";  
+    "https://docs.google.com/forms/d/e/1FAIpQLSfACZzhpliXiEolrF0IDf89XFW_RHx7DaSZkDeDLLF618HE1A/viewform";
   return (
     <>
       <Banner imagePath={bannerImage} title="Donations" />
