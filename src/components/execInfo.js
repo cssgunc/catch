1a9b491fed37bded384cad401177ae0a105e7b66
Bernie Chen <96664333+bcscc@users.noreply.github.com>
2023-11-07 18:42:48 -0500

    Complete execInfo

---
+++ src/components/execInfo.js
@@ -0,0 +1,22 @@
+import presidentImage from '../images/About/execf23/bryce-headshot.jpg'
+import vicePresidentImage from '../images/About/execf23/katieHeadshot.jpg'
+import chiefTechnicalOfficerImage from '../images/About/execf23/IMG_5285.jpg'
+import treasurerImage from '../images/About/execf23/mel-headshot.png'
+import secretaryImage from '../images/About/execf23/IMG_4302.jpg'
+import coChiefQualityOfficerImage1 from '../images/About/execf23/thumbnail_IMG_0796.jpg'
+import coChiefQualityOfficerImage2 from '../images/About/execf23/nal-headshot.png'
+import marketingLead from '../images/About/execf23/IMG_4904.jpg'
+import outreachChair from '../images/About/execf23/IMG_3924.jpg'
+
+
+export const execInfo = [
+    {image: presidentImage, name: "Bryce Womble", position: "President"},
+    {image: vicePresidentImage, name: "Katie Chai", position: "Vice President"},
+    {image: chiefTechnicalOfficerImage, name: "Jacob Ewoldt", position: "Chief Technical Officer"},
+    {image: treasurerImage, name: "Melanie Gall", position: "Treasurer"},
+    {image: secretaryImage, name: "Hieu Doan", position: "Secretary"},
+    {image: coChiefQualityOfficerImage1, name: "Jun Ikeda", position: "Co-Chief Quality Officer"},
+    {image: coChiefQualityOfficerImage2, name: "Nalaya Giraud", position: "Co-Chief Quality Officer"},
+    {image: marketingLead, name: "Maddy Vinal", position: "Marketing Lead"},
+    {image: outreachChair, name: "Bryce Womble", position: "Outreach Chair"}
+]
