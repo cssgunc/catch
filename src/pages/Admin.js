8a14e871db5ee63305ce1958aea38e9e3519e6a3
Denthin <christinehu.read@gmail.com>
2023-11-13 22:08:27 -0500

    Complete Executives Table

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useState } from 'react';
 //import { getAuth } from 'firebase/auth';
+import { db } from '../firebase-config.js';
+import { collection } from 'firebase/firestore';
 import ExitImg from '../images/General/exitDoor.png';
 
 import './Admin.css';
@@ -9,6 +11,7 @@ export default function Admin() {
   //const currUserName = getAuth().currentUser.displayName;
   const currUserName = "John Doe";
   const [currTab, setCurrTab] = useState("Executives");
+  const execRef = collection(db, 'Executives');
 
   function logout() {
     //Insert code here
@@ -16,8 +19,6 @@ export default function Admin() {
   }
 
   function change_tab(tab) {
-    //Insert code here
-    console.log("User is currently switching to " + tab);
     setCurrTab(tab);
   }
 
@@ -33,11 +34,76 @@ export default function Admin() {
     }
   }
 
+  function ExecTable() {
+    const [data, setData] = useState([
+      { id: 1, name: 'Jane Doe', position: 'President', image: 'https://example.com/johndoe.jpg' },
+      { id: 2, name: 'John Doe', position: 'Vice President', image: 'https://example.com/janedoe.jpg' },
+      // Add more rows as needed
+    ]);
+
+    const [editIndex, setEditIndex] = useState(null);
+    const [editedData, setEditedData] = useState({ name: '', position: '', image: '' });
+    const handleEdit = (index) => {
+      setEditIndex(index);
+      setEditedData({ ...data[index] });
+    };
+
+    const handleSave = (index) => {
+      const newData = [...data];
+      newData[index] = editedData;
+      setData(newData);
+      setEditIndex(null);
+    };
+
+    const handleDelete = (index) => {
+      const newData = [...data];
+      newData.splice(index, 1);
+      setData(newData);
+    };
+
+    const handleInputChange = (field, value) => {
+      setEditedData((prevData) => ({ ...prevData, [field]: value }));
+    };
+
+    return (
+      <table className="view-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Picture</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, index) => (
+            <tr key={row.id}>
+              <td>{editIndex === index ? <input type="text" value={editedData.name} onChange={(e) => handleInputChange('name', e.target.value)} /> : row.name}</td>
+              <td>{editIndex === index ? <input type="text" value={editedData.position} onChange={(e) => handleInputChange('position', e.target.value)} /> : row.position}</td>
+              <td>{editIndex === index ? <input type="text" value={editedData.image} onChange={(e) => handleInputChange('image', e.target.value)} /> : row.image}</td>
+              <td>
+                {editIndex === index ? (
+                  <button onClick={() => handleSave(index)} className="view-button">Save</button>
+                ) : (
+                  <>
+                    <button onClick={() => handleEdit(index)} className="view-button">Edit</button>
+                    <button onClick={() => handleDelete(index)} className="view-button">Delete</button>
+                  </>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
+
+
   function RightView() {
     switch (currTab) {
       case "Executives":
         return (
-          <p>Insert Executives Table Here</p>
+          <ExecTable />
         )
       case "Toys":
         return (
