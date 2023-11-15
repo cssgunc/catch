86a4a1f995254dffafcc8822f4eeefdf17bc3df1
Andrew Liu <111478660+ahl1u@users.noreply.github.com>
2023-11-14 19:37:25 -0500

    created dummy login with navbar temp button (#66)

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,7 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { auth } from '../firebase-config'; 
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
-export default function Admin() {
+function Admin() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [loginError, setLoginError] = useState('');
+
+  const adminLogin = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      setIsLoggedIn(true);
+      setLoginError('');
+    } catch (error) {
+      setLoginError('Login failed: ' + error.message);
+      console.error("Error signing in with email and password", error);
+    }
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
-    <div>Admin</div>
-  )
+    <div>
+      {!isLoggedIn ? (
+        <div>
+          <input 
+            type="email" 
+            value={email} 
+            onChange={handleEmailChange} 
+            placeholder="Email" 
+          />
+          <input 
+            type="password" 
+            value={password} 
+            onChange={handlePasswordChange} 
+            placeholder="Password" 
+          />
+          <button onClick={adminLogin}>Login</button>
+          {loginError && <p>{loginError}</p>}
+        </div>
+      ) : (
+        <div>
+          <p>Welcome to the Admin Dashboard!</p>
+          {/* more here later */}
+        </div>
+      )}
+    </div>
+  );
 }
+
+export default Admin;
