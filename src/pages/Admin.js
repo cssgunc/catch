4e6581d0d4ad2eab4546916a5a5cabb136e0a585
Denthin <christinehu.read@gmail.com>
2023-11-16 15:25:48 -0500

    Abstract ExecTable to Table, Create all views except Recent Events

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -18,6 +18,27 @@ export default function Admin() {
     { id: 1, name: 'Jane Doe', position: 'President', image: 'https://example.com/johndoe.jpg' },
     { id: 2, name: 'John Doe', position: 'Vice President', image: 'https://example.com/janedoe.jpg' },
   ]);
+  const [slideshowData, setSlideshowData] = useState([
+    { id: 1, imageId: '8437132145', altText: 'Building a Dinosaur!'},
+    { id: 2, imageId: '1834972562', altText: 'Group Photo'},
+  ]);
+  const [recentToysData, setRecentToysData] = useState([
+    { id: 1, description: 'temp airplane description', name: 'Airplane', imageId: 'airplane', altText: 'Modified Airplane Toy', buildUrl: 'https://docs.google.com/presentation/d/1sG6zYR71rNoACMY5j51roubwaqilKjNm_EgJfxFn7VU/edit#slide=id.p' },
+    { id: 2, description: 'temp bus description', name: 'School Bus', imageId: 'bus', altText: 'Modified Bus Toy', buildUrl: 'bus URL' },
+  ]);
+  const [oldToysData, setOldToysData] = useState([
+    { id: 1, description: 'temp alien description', name: 'Alien', imageId: 'alien', altText: 'Modified Alien Toy', buildUrl: 'alien URL' },
+    { id: 2, description: 'temp dog description', name: 'Dog', imageId: 'dog', altText: 'Modified Dog Toy', buildUrl: 'dog URL' },
+  ]);
+  const [donationsData, setDonationsData] = useState([
+    { id: 1, imageId: 'org6image', altText: 'aversboro alt text', organization: 'Aversboro Elementary School', totalDonations: 10, donationNumber: 1, description: 'aversboro description' },
+    { id: 2, imageId: 'org3image', altText: 'levine alt text', organization: "Levine Children's Hospital", totalDonations: 32, donationNumber: 2, description: 'levine description' },
+  ]);
+  const [mediaData, setMediaData] = useState([
+    { id: 1, imageId: 'image id 1', altText: 'alt text 1', header: 'header 1', description: 'description 1', articleUrl: 'article URL 1' },
+    { id: 2, imageId: 'image id 2', altText: 'alt text 2', header: 'header 2', description: 'description 2', articleUrl: 'article URL 2' },
+  ]);
+
 
   function logout() {
     //Insert code here
@@ -73,17 +94,25 @@ export default function Admin() {
     setEditedData((prevData) => ({ ...prevData, [field]: value }));
   };
  
- 
-  function ExecTable() {
-    const [editIndex, setEditIndex] = useState(null);
-    const [editedData, setEditedData] = useState({ name: '', position: '', image: '' });
 
-    const execHandleAddInit = () => {
+  function Table(props) {
+    const initial_state = props.initial_state;
+    const data = props.data;
+    const setData = props.setData;
+    const headers = props.headers;
+
+    const [editIndex, setEditIndex] = useState(null);
+    const [editedData, setEditedData] = useState(initial_state);
+    const init_keys = Object.keys(initial_state);
+
+    const handleAddInit = () => {
       setEditIndex('plus');
-      setEditedData({ name: '', position: '', image: '' });
+      setEditedData(initial_state);
     }
 
-    const execHandleChange = (field, value) => {
+    const handleChange = (field, value) => {
+      console.log(field);
+      console.log(value);
       return handleInputChange(field, value, setEditedData);
     };
  
@@ -91,28 +120,26 @@ export default function Admin() {
       <table className="view-table">
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Position</th>
-            <th>Picture</th>
+            {headers.map(header => <th key={header}>{header}</th>)}
             <th />
           </tr>
         </thead>
         <tbody>
-          {execData.map((row, index) => (
+          {data.map((row, index) => (
             <tr key={row.id}>
-              <td>{editIndex === index ? <input type="text" value={editedData.name} onChange={(e) => execHandleChange('name', e.target.value)} /> : row.name}</td>
-              <td>{editIndex === index ? <input type="text" value={editedData.position} onChange={(e) => execHandleChange('position', e.target.value)} /> : row.position}</td>
-              <td>{editIndex === index ? <input type="text" value={editedData.image} onChange={(e) => execHandleChange('image', e.target.value)} /> : row.image}</td>
+              {init_keys.map((field) => (
+                <td key={field}>{editIndex === index ? <input type="text" value={editedData[field]} onChange={(e) => handleChange(field, e.target.value)} /> : row[field]}</td>
+              ))}
               <td>
                 {editIndex === index ? (
                   <>
-                    <button onClick={() => handleSave(index, execData, editedData, setExecData, setEditIndex)} className="view-button">Save</button>
+                    <button onClick={() => handleSave(index, data, editedData, setData, setEditIndex)} className="view-button">Save</button>
                     <button onClick={() => handleCancel(setEditIndex)} className="view-button">Cancel</button>
                   </>
                 ) : (
                   <>
-                    <button onClick={() => handleEdit(index, execData, setEditIndex, setEditedData)} className="view-button">Edit</button>
-                    <button onClick={() => handleDelete(index, execData, setExecData)} className="view-button">Delete</button>
+                    <button onClick={() => handleEdit(index, data, setEditIndex, setEditedData)} className="view-button">Edit</button>
+                    <button onClick={() => handleDelete(index, data, setData)} className="view-button">Delete</button>
                   </>
                 )}
               </td>
@@ -120,19 +147,20 @@ export default function Admin() {
           ))}
           {editIndex === 'plus' ? (
             <tr>
-              <td><input type="text" value={editedData.name} onChange={(e) => execHandleChange('name', e.target.value)} /></td>
-              <td><input type="text" value={editedData.position} onChange={(e) => execHandleChange('position', e.target.value)} /></td>
-              <td><input type="text" value={editedData.image} onChange={(e) => execHandleChange('image', e.target.value)} /></td>
+              {init_keys.map((field) => (
+                <td key={field}><input type="text" value={editedData[field]} onChange={(e) => handleChange(field, e.target.value)} /></td>
+              ))}
               <td>
                 <>
-                  <button onClick={() => handleAdd(execData, editedData, setExecData, setEditIndex)} className="view-button">Add</button>
+                  <button onClick={() => handleAdd(data, editedData, setData, setEditIndex)} className="view-button">Add</button>
                   <button onClick={() => handleCancel(setEditIndex)} className="view-button">Cancel</button>
                 </>
               </td>
             </tr> 
           ) : (
             <tr>
-              <td /> <td /> <td /> <td><button onClick={() => execHandleAddInit()} className="add-button">+</button></td>
+              {init_keys.map((field) => <td key={field}/>)}
+              <td><button onClick={handleAddInit} className="add-button">+</button></td>
             </tr>
           )}
         </tbody>
@@ -142,34 +170,61 @@ export default function Admin() {
 
 
   function RightView() {
+    const initializeVals = (obj) => {
+      let newObj = {...obj};
+      delete newObj.id;
+      for (const field of Object.keys(newObj)) {
+        if ((typeof obj[field]) === "string") {
+          newObj[field] = '';
+        } else if ((typeof obj[field]) === "number") {
+          newObj[field] = 0;
+        } else {
+          newObj[field] = null;
+        }
+      }
+      return newObj;
+    } 
     switch (currTab) {
       case "Executives":
+        const execInit = initializeVals(execData[0]);
+        console.log(execInit);
+        const execHeaders = ['Name', 'Position', 'Picture'];
         return (
-          <ExecTable />
+          <Table initial_state={execInit} data={execData}  setData={setExecData} headers={execHeaders} />
         )
       case "Main Slideshow":
+        const slideInit = initializeVals(slideshowData[0]);
+        const slideHeaders = ['Image ID', 'Alternate Text'];
         return (
-          <p>Insert Main Slideshow View here</p>
+          <Table initial_state={slideInit} data={slideshowData}  setData={setSlideshowData} headers={slideHeaders} />
         )
       case "Recent Events":
         return (
           <p>Insert Recent Events View Here</p>
         )
       case "Recent Toys":
-        return(
-          <p>Insert Recent Toys View Here</p>
+        const recentToysInit = initializeVals(recentToysData[0]);
+        const recentToysHeaders = ['Description', 'Name', 'Image ID', 'Alternate Text', 'Build URL'];
+        return (
+          <Table initial_state={recentToysInit} data={recentToysData}  setData={setRecentToysData} headers={recentToysHeaders} />
         )
       case "Old Toys":
+        const oldToysInit = initializeVals(oldToysData[0]);
+        const oldToysHeaders = ['Description', 'Name', 'Image ID', 'Alternate Text', 'Build URL'];
         return (
-          <p>Insert Old Toys View Here</p>
+          <Table initial_state={oldToysInit} data={oldToysData}  setData={setOldToysData} headers={oldToysHeaders} />
         )
       case "Donations":
+        const donationsInit = initializeVals(donationsData[0]);
+        const donationsHeaders = ['Image ID', 'Alternate Text', 'Organization', 'Total Donations', 'Number of Donations', 'Description'];
         return (
-          <p>Insert Donations View Here</p>
+          <Table initial_state={donationsInit} data={donationsData}  setData={setDonationsData} headers={donationsHeaders} />
         )
       case "Media":
+        const mediaInit = initializeVals(mediaData[0]);
+        const mediaHeaders = ['Image ID', 'Alternate Text', 'Header', 'Description', 'Article URL'];
         return (
-          <p>Insert Media View Here</p>
+          <Table initial_state={mediaInit} data={mediaData}  setData={setMediaData} headers={mediaHeaders} />
         )
       default:
         return (
