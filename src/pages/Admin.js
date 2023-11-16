198e19bb33f3868dab1220c3f630899837827377
Denthin <christinehu.read@gmail.com>
2023-11-16 11:39:09 -0500

    Create Add button, move generic handle functions out of ExecTable

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -34,37 +34,58 @@ export default function Admin() {
     }
   }
 
+  const handleEdit = (index, data, setEditIndex, setEditedData) => {
+    setEditIndex(index);
+    setEditedData({ ...data[index] });
+  };
+ 
+  const handleSave = (index, data, editedData, setData, setEditIndex) => {
+    const newData = [...data];
+    newData[index] = editedData;
+    setData(newData);
+    setEditIndex(null);
+  };
+ 
+  const handleDelete = (index, data, setData) => {
+    const newData = [...data];
+    newData.splice(index, 1);
+    setData(newData);
+  };
+
+  const handleAdd = (data, editedData, setData, setEditIndex) => {
+    const newData = [...data, editedData];
+    setData(newData);
+    setEditIndex(null);
+  }
+
+  const handleCancel = (setEditIndex) => {
+    setEditIndex(null);
+  }
+ 
+  const handleInputChange = (field, value, setEditedData) => {
+    setEditedData((prevData) => ({ ...prevData, [field]: value }));
+  };
+ 
+ 
   function ExecTable() {
     const [data, setData] = useState([
       { id: 1, name: 'Jane Doe', position: 'President', image: 'https://example.com/johndoe.jpg' },
       { id: 2, name: 'John Doe', position: 'Vice President', image: 'https://example.com/janedoe.jpg' },
       // Add more rows as needed
     ]);
-
+ 
     const [editIndex, setEditIndex] = useState(null);
     const [editedData, setEditedData] = useState({ name: '', position: '', image: '' });
-    const handleEdit = (index) => {
-      setEditIndex(index);
-      setEditedData({ ...data[index] });
-    };
 
-    const handleSave = (index) => {
-      const newData = [...data];
-      newData[index] = editedData;
-      setData(newData);
-      setEditIndex(null);
-    };
+    const execHandleAddInit = () => {
+      setEditIndex('plus');
+      setEditedData({ name: '', position: '', image: '' });
+    }
 
-    const handleDelete = (index) => {
-      const newData = [...data];
-      newData.splice(index, 1);
-      setData(newData);
+    const execHandleChange = (field, value) => {
+      return handleInputChange(field, value, setEditedData);
     };
-
-    const handleInputChange = (field, value) => {
-      setEditedData((prevData) => ({ ...prevData, [field]: value }));
-    };
-
+ 
     return (
       <table className="view-table">
         <thead>
@@ -72,28 +93,45 @@ export default function Admin() {
             <th>Name</th>
             <th>Position</th>
             <th>Picture</th>
-            <th></th>
+            <th />
           </tr>
         </thead>
         <tbody>
           {data.map((row, index) => (
             <tr key={row.id}>
-              <td>{editIndex === index ? <input type="text" value={editedData.name} onChange={(e) => handleInputChange('name', e.target.value)} /> : row.name}</td>
-              <td>{editIndex === index ? <input type="text" value={editedData.position} onChange={(e) => handleInputChange('position', e.target.value)} /> : row.position}</td>
-              <td>{editIndex === index ? <input type="text" value={editedData.image} onChange={(e) => handleInputChange('image', e.target.value)} /> : row.image}</td>
+              <td>{editIndex === index ? <input type="text" value={editedData.name} onChange={(e) => execHandleChange('name', e.target.value)} /> : row.name}</td>
+              <td>{editIndex === index ? <input type="text" value={editedData.position} onChange={(e) => execHandleChange('position', e.target.value)} /> : row.position}</td>
+              <td>{editIndex === index ? <input type="text" value={editedData.image} onChange={(e) => execHandleChange('image', e.target.value)} /> : row.image}</td>
               <td>
                 {editIndex === index ? (
-                  <button onClick={() => handleSave(index)} className="view-button">Save</button>
+                  <button onClick={() => handleSave(index, data, editedData, setData, setEditIndex)} className="view-button">Save</button>
                 ) : (
                   <>
-                    <button onClick={() => handleEdit(index)} className="view-button">Edit</button>
-                    <button onClick={() => handleDelete(index)} className="view-button">Delete</button>
-                  </>
-                )}
-              </td>
-            </tr>
-          ))}
-        </tbody>
+                  <button onClick={() => handleEdit(index, data, setEditIndex, setEditedData)} className="view-button">Edit</button>
+                  <button onClick={() => handleDelete(index, data, setData)} className="view-button">Delete</button>
+                </>
+              )}
+            </td>
+          </tr>
+        ))}
+        {editIndex === 'plus' ? (
+          <tr>
+            <td><input type="text" value={editedData.name} onChange={(e) => execHandleChange('name', e.target.value)} /></td>
+            <td><input type="text" value={editedData.position} onChange={(e) => execHandleChange('position', e.target.value)} /></td>
+            <td><input type="text" value={editedData.image} onChange={(e) => execHandleChange('image', e.target.value)} /></td>
+            <td>
+              <>
+                <button onClick={() => handleAdd(data, editedData, setData, setEditIndex)} className="view-button">Add</button>
+                <button onClick={() => handleCancel(setEditIndex)} className="view-button">Cancel</button>
+              </>
+            </td>
+          </tr> 
+        ) : (
+          <tr>
+            <td /> <td /> <td /> <td><button onClick={() => execHandleAddInit()} className="add-button">+</button></td>
+          </tr>
+        )}
+       </tbody>
       </table>
     );
   };
@@ -105,9 +143,21 @@ export default function Admin() {
         return (
           <ExecTable />
         )
-      case "Toys":
+      case "Main Slideshow":
         return (
-          <p>Insert Toys View Here</p>
+          <p>Insert Main Slideshow View Here</p>
+        )
+      case "Recent Events":
+        return (
+          <p>Insert Recent Events View Here</p>
+        )
+      case "Recent Toys":
+        return(
+          <p>Insert Recent Toys View Here</p>
+        )
+      case "Old Toys":
+        return (
+          <p>Insert Old Toys View Here</p>
         )
       case "Donations":
         return (
@@ -141,7 +191,10 @@ export default function Admin() {
         <div className="left-body">
           <div className="title">Dashboard</div>
           <Tab tabName="Executives" />
-          <Tab tabName="Toys" />
+          <Tab tabName="Main Slideshow" />
+          <Tab tabName="Recent Events" />
+          <Tab tabName="Recent Toys" />
+          <Tab tabName="Old Toys" />
           <Tab tabName="Donations" />
           <Tab tabName="Media" />
         </div>
