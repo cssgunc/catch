02eb256de872768719e48a3be8ee30a12ebf15ce
Nirmal Rajendran <nirmal@ad.unc.edu>
2023-11-28 19:14:59 -0500

    Fix navbar color change bug

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -168,7 +168,7 @@ function ShoppingCartPanel(props) {
 }
 
 export default function NavBar() {
-  const [activeTab, setActiveTab] = useState('/');
+  const [activeTab, setActiveTab] = useState(window.location.pathname);
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [visible, setVisible] = useState(true);
   const [shoppingCartActive, setShoppingCartActive] = useState(false);
