fdc0363f108671bf90379b18c32e94435a67bfb2
Nithin Sivakumaran <113319591+nsivaku@users.noreply.github.com>
2024-06-18 14:25:37 -0400

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
-console.log(firebaseConfig);
+// console.log(firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
